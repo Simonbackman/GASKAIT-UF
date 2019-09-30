f4896157f3ff4fd74c040b76d921136b1e144013
Simonbackman <42805407+Simonbackman@users.noreply.github.com>
2019-09-30 10:01:08 +0200

    Add files via upload

--- Priser.php
+++ Priser.php
@@ -14,9 +14,58 @@
    <?php include('templates/nav.php');?>
    
    <article class="main">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum fuga maxime reiciendis a, veniam minus explicabo, neque dolorum consequuntur consectetur corporis rem sed maiores voluptatibus repellat repellendus temporibus tempora quidem consequatur laborum ducimus ad tenetur? Quis accusamus iure dolores sunt, unde inventore ut culpa. Sit ducimus quidem eveniet, provident nam. Hic eaque, reprehenderit, magnam quaerat cupiditate illum explicabo minus nesciunt deleniti eius a aut optio maxime ab libero deserunt, neque veniam tempore iure. Delectus eum tempore quae nemo suscipit animi praesentium asperiores dignissimos, rerum tempora quibusdam illum, quod laborum voluptatibus sed nisi at ipsam, optio deserunt error atque culpa similique. Rerum magni atque in nulla quia nobis sit est necessitatibus autem ad laborum iusto consectetur numquam animi accusamus recusandae, maxime cum totam voluptatem sed debitis maiores a laudantium quod. Optio atque voluptatem soluta fugiat beatae vitae sunt a eos neque, odio quia facilis tenetur pariatur similique architecto minima nobis voluptates, praesentium ut eligendi autem, in non doloribus. Suscipit soluta, qui commodi rerum quas voluptate, nisi quod neque alias at impedit repellat aut delectus tenetur. Vitae vero neque aliquam vel fuga laborum doloremque facilis eos amet illo voluptas, dolore fugit libero esse magni velit perspiciatis minus explicabo autem, consequatur, corporis assumenda. Numquam accusantium itaque ab suscipit nobis expedita inventore, illo, quidem ad neque sint perferendis voluptates a culpa blanditiis repellendus esse rerum fuga aperiam quis voluptatum temporibus doloribus sapiente! Quae animi eaque quas molestiae nobis nulla dignissimos, omnis non libero, reprehenderit nam tempore, maiores harum quis temporibus! Perspiciatis debitis quos iste nulla corporis, rem natus autem, fuga nihil reprehenderit temporibus dolorem dignissimos cum nemo? Minima culpa ea, tempora blanditiis rerum labore a non magnam nisi nihil corporis, laborum laudantium! A voluptas quisquam dolor eum deleniti minus, aut, magni nulla odit exercitationem veniam nostrum. Odio vel eligendi similique quasi mollitia, impedit quisquam quis tenetur? Modi hic incidunt ut eveniet. Expedita alias natus saepe reprehenderit error a, deserunt, quaerat iure tempore, nulla dicta quod, aut asperiores inventore. Quibusdam magnam, quisquam eius ipsa eos sequi voluptates tempora doloremque debitis animi, molestiae, non, saepe explicabo! Cupiditate reprehenderit officiis dolorem, soluta vero, sequi debitis repellendus expedita voluptas fugiat ullam illo aliquid officia non amet recusandae modi dicta eos iste, atque maxime ad sint hic. Aut error, voluptatibus obcaecati illo id aliquam, consequatur temporibus recusandae autem omnis sint voluptatem voluptates totam quas aperiam praesentium placeat dolorem quae fugit ab magni nesciunt pariatur? Fugit eius nam quas incidunt.</p> 
-      
-   </article>
+        <div class="price_div">
+            <div class="box1">
+                <h1 class="package_name">Box 1</h1>
+                <div class="package_price">
+                    <p>000<span>KR</span></p>
+                </div>
+                <div class="package_list">
+                    <ul class="package_ul">
+                        <li>test &#9745;</li>
+                        <li>test &#9745;</li>
+                        <li>Mobile Responsive &#9745;</li>
+                        <li>test &#x2612;</li>
+                        <li>test &#x2612;</li>
+                    </ul>
+                </div>
+                <a href="" class="price_btn">test</a>
+            </div>
+            <div class="box2">
+                <h1 class="package_name">box 2</h1>
+                <div class="package_price">
+                     <p>000<span>KR</span></p>
+                </div>
+                <div class="package_list">
+                    <ul class="package_ul">
+                        <li>test </li>
+                        <li>test </li>
+                        <li>Mobile Responsive &#9745;</li>
+                        <li>test </li>
+                        <li>test </li>
+                    </ul>
+                </div>
+                <a href="" class="price_btn">test</a>
+            </div>
+            <div class="box3">
+                <h1 class="package_name">box 3</h1>
+                <div class="package_price">
+                    <p>000<span>KR</span></p>
+                </div>
+                <div class="package_list">
+                    <ul class="package_ul">
+                        <li>test &#9745;</li>
+                        <li>test &#9745;</li>
+                        <li>Mobile Responsive &#9745;</li>
+                        <li>test &#9745;</li>
+                        <li>test &#x2612;</li>
+                    </ul>
+                </div>
+                <a href="" class="price_btn">test</a>
+            </div>
+        </div>
+    
+    </article>
     <?php include('templates/footer.php'); ?>
 </body>
 </html>
