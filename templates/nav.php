d797905461718a6b08dceac900d9a81b38ca1467
Simonbackman <42805407+Simonbackman@users.noreply.github.com>
2019-10-27 15:52:52 +0100

    Update nav.php

--- templates/nav.php
+++ templates/nav.php
@@ -1,26 +1,27 @@
 <nav>
-        <div class="logo">
-<!--            <h4>GASKAIT-UF</h4>-->
-           <img class="nav-logo" src="image/gaskalogo.png" alt="logo">
-        </div>
-        <ul class="nav-links">
-            <li><a href="index.php">
-                    <div class="nav-item"><i class="fas fa-home"></i> Home</div>
-                </a></li>
-            <li><a href="priser.php">
-                    <div class="nav-item">Priser</div>
-                </a></li>
-            <li><a href="support.php">
-                    <div class="nav-item">Support</div>
-                </a></li>
-            <li><a href="OmOss.php">
-                    <div class="nav-item">OmOss</div>
-                </a></li>
-        </ul>
-        <div class="burger">
-            <div class="line1"></div>
-            <div class="line2"></div>
-            <div class="line3"></div>
-        </div>
-        
-    </nav>
+    <div class="logo">
+        <!--            <h4>GASKAIT-UF</h4>-->
+        <img class="nav-logo" src="image/gaskalogo.png" alt="logo">
+    </div>
+    <ul class="nav-links">
+        <li><a href="index.php">
+                <div class="nav-item"><i class="fas fa-home"></i> Home</div>
+            </a>
+        </li>
+        <li><a href="priser.php">
+                <div class="nav-item">Priser</div>
+            </a></li>
+        <li><a href="support.php">
+                <div class="nav-item">Support</div>
+            </a></li>
+        <li><a href="OmOss.php">
+                <div class="nav-item">OmOss</div>
+            </a></li>
+    </ul>
+    <div class="burger">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+    </div>
+
+</nav>
