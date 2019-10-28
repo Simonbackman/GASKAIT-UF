4fe11d41dda9f90ffcded4a6a3bbb1fa523889e9
Simonbackman <42805407+Simonbackman@users.noreply.github.com>
2019-10-28 02:25:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
    <?php include('templates/nav.php')?>
     
    <div class="container">
-    	<div class="other-content">Need to find free images/no copyright</div>
+    	<div class="other-content">Need to find free images/no copyright And if we can keep the slider</div>
     	<div class="slide show" style="background-image: url('image/codeimg.webp');"></div>
     	<div class="slide" style="background-image: url('image/code-1076533_960_720.jpg');"></div>
     	<div class="slide" style="background-image: url('image/code-2434271_960_720.jpg');"></div>
@@ -26,7 +26,7 @@
         <div class="main_page_txt_div">
             <h1>test</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing el Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere eligendi officia accusantium distinctio quia? Obcaecati, voluptate exercitationem. Maxime, mollitia iste repellendus corporis, placeat necessitatibus excepturi nobis atque, corrupti cum perferendis nihil sed. Vero facere voluptate nemo rerum ex at iusto adipisci quos, libero accusamus repudiandae molestias harum pariatur vel blanditiis explicabo ipsum commodi nobis aliquam ratione ea aut corporis! Voluptates soluta natus et laborum odio voluptatibus excepturi cupiditate at repellat, modi magni. Ad accusantium id itaque eaque cumque, repudiandae pariatur? Error autem quibusdam velit optio ad explicabo iure, blanditiis mollitia praesentium nemo dolore dolores atque amet eius cum dignissimos molestias. Quo quidem minima neque debitis harum aperiam alias obcaecati! Tempora, quaerat quis? Voluptatem voluptates cum aliquid nulla aut commodi delectus, ex molestiae, accusamus illum expedita culpa velit, cupiditate impedit quidem reiciendis quas officiis quasi omnis! Beatae velit est tenetur maxime saepe sequi, dolore porro, aut error laudantium adipisci! Dolor doloremque delectus consequuntur maxime quasi enim ad totam odio expedita pariatur necessitatibus natus hic impedit iure, numquam ratione. Ab error cumque voluptatem possimus. Repellendus magnam quidem voluptas fugit dolor quaerat nobis! Deleniti, quia consectetur, doloribus eius numquam expedita voluptate qui assumenda maiores ducimus dolores itaque in maxime quo beatae voluptas adipisci. it. Expedita ipsum cumque ducimus eos dolorum perspiciatis ab tenetur pariatur ullam amet reiciendis eum obcaecati vero, doloribus blanditiis veritatis iusto illo magnam nisi fugiat, eligendi eveniet alias quod. Et reprehenderit explicabo ad veniam vero modi, accusantium ea quae aperiam voluptas, architecto dignissimos.</p>
-            <h1>IF IT LAGS ON MAIN PAGE CONSIDER REMOVING SO THE NAV BAR DON'T FOLLOW AS YOU SCROLL</h1>
+            <h1>IF IT LAGS ON MAIN PAGE CONSIDER REMOVING  THE image slider </h1>
         </div>
     </article>
 
